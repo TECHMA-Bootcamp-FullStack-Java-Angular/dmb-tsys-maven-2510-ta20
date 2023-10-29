8af22cae7c0de79b56792bab38f87cb0c110a1fb
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-29 14:26:24 +0100

    add docs directory

---
+++ docs/js/lessons.js
@@ -0,0 +1,99 @@
+export const lessons = [
+  {
+    numLessons: 4,
+    issue: 'Maven',
+    day: 32,
+    msg: 'El mejor reto en la vida <br>Es ser la mejor version de tí cada dia',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 1,
+        title: '',
+        description:`
+        Intenta escribir una aplicación con interfaz gráfica en la que se construya una
+        ventana con título y marco que tenga los controles básico (es decir, restaurar,
+        maximizar y cerrar) y que al pulsar sobre el aspa de la ventana (cerrar) se salga completamente de la aplicación. 
+        <br>La ventana contendrá una etiqueta y el usuario debe poder cambiar su tamaño.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_01/Main_EJ01.java',
+        urlPG: '',
+      },
+      {
+        numExercise: 2,
+        title: '',
+        description: `
+        Escribe una aplicación gráfica con una ventana que tenga una etiqueta y dos botones de operación.
+        <br>El comportamiento de la aplicación debe reflejar en el texto de la etiqueta cuál es el último botón en el que el usuario ha hecho clic.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_02/Main_EJ02.java',
+        urlPG: ''
+      },
+      {
+        numExercise: 3,
+        title: '',
+        description: `Intenta escribir una aplicación gráfica con una ventana que tenga dos etiquetas y dos
+        botones de operación.<br> El comportamiento de la aplicación debe reflejar en el texto
+        de las etiquetas el número de veces que el usuario ha hecho clic en cada uno de los
+        botones`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_03/Main_EJ03.java',
+        urlPG: ''
+      },
+      {
+        numExercise: 4,
+        title: '',
+        description: `Intenta escribir una aplicación gráfica con una ventana que tenga una etiqueta y un
+        área de texto. <br> La aplicación debe reflejar en el área de texto todos los eventos de
+        ventana que se produzcan por la creación de la ventana o por las interacciones del
+        usuario.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_04/Main_EJ04.java',
+        urlPG: ''
+      },
+      {
+        numExercise: 5,
+        title: '',
+        description: `
+        Escribe una aplicación gráfica con una ventana que tenga un botón y un área de
+        texto.  <br> La aplicación debe reflejar en el área de texto los principales eventos de ratón
+        que se produzcan sobre dicha área por las interacciones del usuario. <br>Haciendo clic en
+        el botón se limpiará el contenido del área de texto`, 
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_05/Main_EJ05.java',
+        urlPG: ''
+      },
+      {
+        numExercise: 6,
+        title: '',
+        description: `
+        Intenta escribir una aplicación gráfica que permita calcular el índice de masa
+        corporal. <br> Os recuerdo que este índice se calcula dividiendo el peso de una persona en
+        kilos por el cuadrado de su altura en metros.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_06/Main_EJ06.java',
+        urlPG: ''
+      },
+      {
+        numExercise: 7,
+        title: '',
+        description: `
+        Intenta escribir una aplicación gráfica que permita de forma sencilla realizar el
+        cambio de pesetas a euros (y viceversa).<br> Por si acaso, os recuerdo que la tasa de
+        cambio que se aplica es 1 euro a 166,386 pesetas.<br> En todo momento, el usuario debe
+        estar informado de la conversión que se está realizando`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_07/Main_EJ07.java',
+        urlPG: ''
+      },
+      {
+        numExercise: 8,
+        title: '',
+        description: `Intenta mejorar la aplicación gráfica del ejercicio anterior. <br>Además de las
+        funcionalidades anteriores, se debe permitir operar con los botones desde el teclado
+        y hay que añadir un botón que permita borrar los campos de datos y resultado.<br>
+        También para darle mayor robustez, puedes incluir un control de errores que avise
+        mediante una ventana emergente si se ha introducido un número en formato
+        erróneo, evitando que la aplicación se detenga.`,
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_08/Main_EJ08.java',
+        urlPG: ''
+      },
+    ]
+  },
+]
+
+
+ 
