9f9110334005094d61dbc0dc47492e2a10df3bdf
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-29 14:33:13 +0100

    add new readme

--- docs/js/lessons.js
+++ docs/js/lessons.js
@@ -15,8 +15,8 @@ export const lessons = [
         ventana con título y marco que tenga los controles básico (es decir, restaurar,
         maximizar y cerrar) y que al pulsar sobre el aspa de la ventana (cerrar) se salga completamente de la aplicación. 
         <br>La ventana contendrá una etiqueta y el usuario debe poder cambiar su tamaño.`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_01/Main_EJ01.java',
-        urlPG: '',
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_01/Ejercicio_01.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-1.png?raw=true',
       },
       {
         numExercise: 2,
@@ -24,8 +24,8 @@ export const lessons = [
         description: `
         Escribe una aplicación gráfica con una ventana que tenga una etiqueta y dos botones de operación.
         <br>El comportamiento de la aplicación debe reflejar en el texto de la etiqueta cuál es el último botón en el que el usuario ha hecho clic.`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_02/Main_EJ02.java',
-        urlPG: ''
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_02/Ejercicio_02.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-2.png?raw=true'
       },
       {
         numExercise: 3,
@@ -34,8 +34,8 @@ export const lessons = [
         botones de operación.<br> El comportamiento de la aplicación debe reflejar en el texto
         de las etiquetas el número de veces que el usuario ha hecho clic en cada uno de los
         botones`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_03/Main_EJ03.java',
-        urlPG: ''
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_03/Ejercicio_03.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-3.png?raw=true'
       },
       {
         numExercise: 4,
@@ -44,8 +44,8 @@ export const lessons = [
         área de texto. <br> La aplicación debe reflejar en el área de texto todos los eventos de
         ventana que se produzcan por la creación de la ventana o por las interacciones del
         usuario.`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_04/Main_EJ04.java',
-        urlPG: ''
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_04/Ejercicio_04.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_04/Main_EJ04.java'
       },
       {
         numExercise: 5,
@@ -55,8 +55,8 @@ export const lessons = [
         texto.  <br> La aplicación debe reflejar en el área de texto los principales eventos de ratón
         que se produzcan sobre dicha área por las interacciones del usuario. <br>Haciendo clic en
         el botón se limpiará el contenido del área de texto`, 
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_05/Main_EJ05.java',
-        urlPG: ''
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_05/Ejercicio_05.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-5.png?raw=true'
       },
       {
         numExercise: 6,
@@ -65,8 +65,8 @@ export const lessons = [
         Intenta escribir una aplicación gráfica que permita calcular el índice de masa
         corporal. <br> Os recuerdo que este índice se calcula dividiendo el peso de una persona en
         kilos por el cuadrado de su altura en metros.`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_06/Main_EJ06.java',
-        urlPG: ''
+        urlCode: 'hhttps://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_06/Ejercicio_06.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-6.png?raw=true'
       },
       {
         numExercise: 7,
@@ -76,8 +76,8 @@ export const lessons = [
         cambio de pesetas a euros (y viceversa).<br> Por si acaso, os recuerdo que la tasa de
         cambio que se aplica es 1 euro a 166,386 pesetas.<br> En todo momento, el usuario debe
         estar informado de la conversión que se está realizando`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_07/Main_EJ07.java',
-        urlPG: ''
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_07/Ejercicio_07.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-7.png?raw=true'
       },
       {
         numExercise: 8,
@@ -88,8 +88,8 @@ export const lessons = [
         También para darle mayor robustez, puedes incluir un control de errores que avise
         mediante una ventana emergente si se ha introducido un número en formato
         erróneo, evitando que la aplicación se detenga.`,
-        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-0310-ta06/blob/main/src/com/tmbs/ta06_08/Main_EJ08.java',
-        urlPG: ''
+        urlCode: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/src/main/java/ejercicio_08/Ejercicio_08.java',
+        urlPG: 'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-maven-2510-ta20/blob/main/docs/assets/20-8.png?raw=true'
       },
     ]
   },
